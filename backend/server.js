83e17c18bb7236a5e54333040bf006ee61a6087b
raihamza86 <rahamza146@gmail.com>
2025-07-12 11:35:00 +0500

    connect frontend with backend api

--- backend/server.js
+++ backend/server.js
@@ -7,12 +7,12 @@ const urlRoutes = require('./routes/urlRoutes');
 dotenv.config();
 const app = express();
 
-const corsOptions = {
-    origin: process.env.FRONTEND_URL,
+app.use(cors({
+    origin: "http://localhost:5173",
     credentials: true,
-};
+    methods: ["GET", "POST", "PUT", "DELETE"]
+}));
 
-app.use(cors(corsOptions));
 app.use(express.json());
 app.use('/shorten', urlRoutes);
 
